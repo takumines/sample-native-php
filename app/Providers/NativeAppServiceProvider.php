6f1fb845aeef13491924056ab5efccd930d7989b
takumines <takumines1@gmail.com>
2023-07-28 11:22:40 +0900

    色々触ってみたの巻

--- app/Providers/NativeAppServiceProvider.php
+++ app/Providers/NativeAppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Native\Laravel\Dialog;
 use Native\Laravel\Facades\ContextMenu;
 use Native\Laravel\Facades\Dock;
+use Native\Laravel\Facades\MenuBar;
+use Native\Laravel\Facades\Notification;
 use Native\Laravel\Facades\Window;
 use Native\Laravel\Facades\GlobalShortcut;
 use Native\Laravel\Menu\Menu;
@@ -26,6 +29,15 @@ class NativeAppServiceProvider
                 ->toggleFullscreen()
                 ->separator()
                 ->link('https://laravel.com', 'Learn More', 'CmdOrCtrl+L')
+                ->quit()
+            )
+            ->submenu('Page', Menu::new()
+                ->link(route('atti'), 'あっち')
+                ->separator()
+                ->link(route('kotti'), 'こっち')
+            )
+            ->submenu('Company', Menu::new()
+                ->link('https://nomadori.com', 'Nomadori')
             )
             ->register();
 
@@ -33,26 +45,33 @@ class NativeAppServiceProvider
             ->width(800)
             ->height(800);
 
-        /**
-            Dock::menu(
-                Menu::new()
-                    ->event(DockItemClicked::class, 'Settings')
-                    ->submenu('Help',
-                        Menu::new()
-                            ->event(DockItemClicked::class, 'About')
-                            ->event(DockItemClicked::class, 'Learn More…')
-                    )
+        MenuBar::create()
+            ->showDockIcon()
+            ->label('Status: Online');
+
+        Dock::menu(
+            Menu::new()
+                ->event(DockItemClicked::class, 'Settings')
+                ->submenu('Help',
+                    Menu::new()
+                        ->event(DockItemClicked::class, 'About')
+                        ->event(DockItemClicked::class, 'Learn More…')
+                )
             );
 
-            ContextMenu::register(
-                Menu::new()
-                    ->event(ContextMenuClicked::class, 'Do something')
-            );
+        Notification::title('Hello from NativePHP')
+            ->message('This is a detail message coming from your Laravel app.')
+            ->show();
 
-            GlobalShortcut::new()
-                ->key('CmdOrCtrl+Shift+I')
-                ->event(ShortcutPressed::class)
-                ->register();
-        */
+        ContextMenu::register(
+            Menu::new()
+                ->event(ContextMenuClicked::class, 'Do something')
+        );
+
+        GlobalShortcut::new()
+            ->key('CmdOrCtrl+Shift+I')
+            ->event(ShortcutPressed::class)
+            ->register();
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+Route::get('/atti', function () {
+    return view('atti');
+})->name('atti');
+Route::get('/kotti', function () {
+    return view('kotti');
+})->name('kotti');
